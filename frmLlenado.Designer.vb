dc2341bf7b0c9b71396d5b46ce0623874b78be30
ecacerest <ecacerest@finagil.com.mx>
2020-10-23 17:44:25 -0500

    ADD TESO_ConfirmaMinistracionesCXP

--- frmLlenado.Designer.vb
+++ frmLlenado.Designer.vb
@@ -22,53 +22,17 @@ Partial Class frmLlenado
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnProcesar = New System.Windows.Forms.Button
-        Me.btnCierre = New System.Windows.Forms.Button
-        Me.Button1 = New System.Windows.Forms.Button
         Me.SuspendLayout()
-        '
-        'btnProcesar
-        '
-        Me.btnProcesar.Location = New System.Drawing.Point(233, 52)
-        Me.btnProcesar.Name = "btnProcesar"
-        Me.btnProcesar.Size = New System.Drawing.Size(75, 23)
-        Me.btnProcesar.TabIndex = 0
-        Me.btnProcesar.Text = "Procesar"
-        Me.btnProcesar.UseVisualStyleBackColor = True
-        '
-        'btnCierre
-        '
-        Me.btnCierre.Location = New System.Drawing.Point(233, 110)
-        Me.btnCierre.Name = "btnCierre"
-        Me.btnCierre.Size = New System.Drawing.Size(75, 23)
-        Me.btnCierre.TabIndex = 1
-        Me.btnCierre.Text = "Cierre"
-        Me.btnCierre.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(233, 166)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Cierre Diario"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'frmLlenado
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(549, 462)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.btnCierre)
-        Me.Controls.Add(Me.btnProcesar)
+        Me.ClientSize = New System.Drawing.Size(393, 59)
         Me.Name = "frmLlenado"
         Me.Text = "Llenado de DetalleFINAGIL"
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents btnProcesar As System.Windows.Forms.Button
-    Friend WithEvents btnCierre As System.Windows.Forms.Button
-    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
